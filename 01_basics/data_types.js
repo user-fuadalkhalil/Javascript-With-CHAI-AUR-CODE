e43ee2b33d33ce9038bc7ddcdcbb8554147ad033
Fuad <167557967+wait-fuad@users.noreply.github.com>
2024-05-29 15:21:35 +0000

    adding data_types.js file

---
+++ 01_basics/data_types.js
@@ -0,0 +1,24 @@
+"user strict"  //treat all js code as newr version
+
+// alert("hello"); // we are using nodejs, not browser, that's why it will make an error
+
+// console.log(3+3); console.log("hello"); //code will run, but it is a bad practice
+
+let name = "fuad"
+let age = 24
+let isLoggedIn = false
+let states = null
+
+
+// number => 2 to power 53
+// bigint => 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undesfined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof "fuad");

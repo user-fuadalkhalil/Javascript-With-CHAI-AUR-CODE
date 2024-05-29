019f3dffb2b44cba24fb0592283be9c2b487583e
Fuad <167557967+wait-fuad@users.noreply.github.com>
2024-05-29 16:13:02 +0000

    operation details

--- 01_basics/conversion_operation.js
+++ 01_basics/conversion_operation.js
@@ -1,3 +1,6 @@
+/***** conversion ****/
+
+
 let score = "33abc"
 
 // console.log(typeof score);
@@ -29,3 +32,34 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 // console.log(stringNumber);
 // console.log(typeof stringNumber);
+
+
+
+/******** operations ********/
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = " fuad"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(3 + 5 / 6 * 7 % 8)  //bad practice
+
+// console.log(+true);  //not allowed
+// console.log(+"");    //not allowed
+
+let gameCounter = 100
+++gameCounter
+// console.log(gameCounter);

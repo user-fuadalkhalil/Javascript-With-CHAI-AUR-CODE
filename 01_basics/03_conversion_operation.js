26d9cd587426431adb0a062679009fc418863e26
Fuad <167557967+wait-fuad@users.noreply.github.com>
2024-08-29 17:09:22 +0000

    Another update in files

--- 01_basics/03_conversion_operation.js
+++ 01_basics/03_conversion_operation.js
@@ -1,65 +1,92 @@
-/***** conversion ****/
+// /***** conversion ****/
 
+
+// let score = "33abc"
+
+// // console.log(typeof score);
+// // console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// // console.log(typeof valueInNumber);
+// // console.log(valueInNumber);
+
+
+// //convert in number
+// // "33" => 33
+// // "33abc" => NaN
+// // true => 1; false => 0
+
+// let isLoggedIn = 1
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// // console.log(booleanIsLoggedIn);
+
+// //boolean conversion
+// //1 => true; 0 => false
+// //"" => false
+// //"fuad" => true
+
+
+// let someNumber = 33
+
+// let stringNumber = String(someNumber)
+// // console.log(stringNumber);
+// // console.log(typeof stringNumber);
+
+
+
+// /******** operations ********/
+
+// // console.log(2+2);
+// // console.log(2-2);
+// // console.log(2*2);
+// // console.log(2/2);
+// // console.log(2**2);
+// // console.log(2%2);
+
+// let str1 = "hello"
+// let str2 = " fuad"
+
+// let str3 = str1 + str2
+// // console.log(str3);
+
+// // console.log("1" + 2);
+// // console.log(1 + "2");
+// // console.log("1" + 2 + 2);
+// // console.log(1 + 2 + "2");
+
+// // console.log(3 + 5 / 6 * 7 % 8)  //bad practice
+
+// // console.log(+true);  //not allowed
+// // console.log(+"");    //not allowed
+
+// let gameCounter = 100
+// ++gameCounter
+// // console.log(gameCounter);
 
 let score = "33abc"
 
-// console.log(typeof score);
-// console.log(typeof(score));
+//console.log(typeof(score));
 
 let valueInNumber = Number(score)
-// console.log(typeof valueInNumber);
-// console.log(valueInNumber);
+//console.log(typeof(valueInNumber));
+//console.log(valueInNumber);
 
-
-//convert in number
-// "33" => 33
-// "33abc" => NaN
-// true => 1; false => 0
+//"33" => 33
+//"33abc" => NaN
+//true => 1, false => 0
 
 let isLoggedIn = 1
-
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-// console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
-//boolean conversion
-//1 => true; 0 => false
+
+//1 => true, 0 => false
 //"" => false
 //"fuad" => true
-
 
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-// console.log(stringNumber);
-// console.log(typeof stringNumber);
-
-
-
-/******** operations ********/
-
-// console.log(2+2);
-// console.log(2-2);
-// console.log(2*2);
-// console.log(2/2);
-// console.log(2**2);
-// console.log(2%2);
-
-let str1 = "hello"
-let str2 = " fuad"
-
-let str3 = str1 + str2
-// console.log(str3);
-
-// console.log("1" + 2);
-// console.log(1 + "2");
-// console.log("1" + 2 + 2);
-// console.log(1 + 2 + "2");
-
-// console.log(3 + 5 / 6 * 7 % 8)  //bad practice
-
-// console.log(+true);  //not allowed
-// console.log(+"");    //not allowed
-
-let gameCounter = 100
-++gameCounter
-// console.log(gameCounter);
+console.log(stringNumber);
+console.log(typeof stringNumber);

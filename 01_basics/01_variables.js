53b33f8d865c74efe2c04f931a8dbaf2b43de9fa
Fuad <167557967+wait-fuad@users.noreply.github.com>
2024-08-29 04:05:16 +0000

    New additions

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,20 +1,42 @@
-const accountId = 11223344
-let accountEmail = "waitfuad@gmail.com"
-let accountPassword = "789654123"
+// const accountId = 11223344
+// let accountEmail = "waitfuad@gmail.com"
+// let accountPassword = "789654123"
+// accountCity = "Dhaka"
+
+
+// // accountId = 332211   // constant overwrite is not allowd
+
+// accountEmail = "fuad09603@gmail.com"
+// accountPassword = "123456789"
+// accountCity = "Bhola"
+
+// // console.log(accountId);
+
+// console.table([accountId, accountEmail, accountPassword, accountCity]); //printing variables as a tabular fom
+
+// /*
+//   prefer not to use var
+//   because of issue in block scope and functional scope
+// */
+
+
+
+const accountId = 144553
+let accountEmail = "waifuad@gmail.com"
+var accountPassword = "20103040"
+accountCity = "Bhola"
+let accountState
+
+// accountId = 2 //constant overwrite is not allowed
+console.log(accountId);
+
+accountEmail = "okfuad@gmail.com"
+accountPassword = "50405030"
 accountCity = "Dhaka"
 
+console.table([accountEmail, accountPassword, accountCity]);
+console.log(accountState);
 
-// accountId = 332211   // constant overwrite is not allowd
 
-accountEmail = "fuad09603@gmail.com"
-accountPassword = "123456789"
-accountCity = "Bhola"
-
-// console.log(accountId);
-
-console.table([accountId, accountEmail, accountPassword, accountCity]); //printing variables as a tabular fom
-
-/*
-  prefer not to use var
-  because of issue in block scope and functional scope
-*/
+/*prefer not to use var
+because of issue in block scope using */

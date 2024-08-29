69a44167d910a729c311a858890ba66bdb801720
Fuad <167557967+wait-fuad@users.noreply.github.com>
2024-08-29 04:28:46 +0000

    Addition again

--- 01_basics/02_data_types.js
+++ 01_basics/02_data_types.js
@@ -1,24 +1,51 @@
-"user strict"  //treat all js code as newr version
+// "user strict"  //treat all js code as newr version
 
-// alert("hello"); // we are using nodejs, not browser, that's why it will make an error
+// // alert("hello"); // we are using nodejs, not browser, that's why it will make an error
 
-// console.log(3+3); console.log("hello"); //code will run, but it is a bad practice
+// // console.log(3+3); console.log("hello"); //code will run, but it is a bad practice
 
-let name = "fuad"
+// let name = "fuad"
+// let age = 24
+// let isLoggedIn = false
+// let states = null
+
+
+// // number => 2 to power 53
+// // bigint => 
+// // string => ""
+// // boolean => true/false
+// // null => standalone value
+// // undesfined => 
+// // symbol => unique
+
+
+// // object
+
+// console.log(typeof "fuad");
+
+
+
+// alert(5 + 5); //we are using node.js, not browser
+
+//code readability must have
+
+let name = "Fuad"
 let age = 24
 let isLoggedIn = false
-let states = null
 
+//primitive data types
 
 // number => 2 to power 53
-// bigint => 
-// string => ""
+// bigint
 // boolean => true/false
+// string => ""
 // null => standalone value
-// undesfined => 
-// symbol => unique
+// undefined => 
+// symbol =>
 
 
-// object
+//object
 
-console.log(typeof "fuad");
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+

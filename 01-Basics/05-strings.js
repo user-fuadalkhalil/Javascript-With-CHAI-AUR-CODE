30f3b700ff07ffb9653608d819f1b971c2a35204
Fuad <167557967+wait-fuad@users.noreply.github.com>
2024-09-01 10:37:07 +0000

    more in the string file

--- 01-Basics/05-strings.js
+++ 01-Basics/05-strings.js
@@ -8,10 +8,32 @@ const repoCount = 50
 // string interpolation
 
 
-const gameName = new String('Fuad')
+const gameName = new String('Fuad-Al-Khalil')
 
-console.log(gameName[0]);
-console.log(gameName.__proto__);
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
 
 
-console.log(gameName.length);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt('4'));
+// console.log(gameName.indexOf('d'));
+
+const newString = gameName.substring(0, 4);
+// console.log(newString);
+
+
+const anotherString = gameName.slice(-8, 4)
+// console.log(anotherString);
+
+const newStringOne = "   Fuad  "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+const url = "https://chatgpt.com/c/b419f9e1%20-c572-4466-b003-70e8f7927478"
+
+// console.log(url.replace('-', 'dash'));
+
+// console.log(url.includes('chatgpt'));
+
+console.log(gameName.split('-'));

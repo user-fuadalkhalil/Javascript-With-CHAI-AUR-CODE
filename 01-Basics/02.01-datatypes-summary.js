7035bc2e39591ea3c043da466f60256803b7f7e8
Fuad <167557967+wait-fuad@users.noreply.github.com>
2024-09-01 05:52:11 +0000

    more about data types

---
+++ 01-Basics/02.01-datatypes-summary.js
@@ -0,0 +1,37 @@
+// Primitive Datatype
+
+// Seven Types: String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+const score = 100
+const scoreOne = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 125646546546465n 
+
+
+
+//Reference (Non Primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+
+let person = {
+    name: "hitesh",
+    age: 25
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof id);

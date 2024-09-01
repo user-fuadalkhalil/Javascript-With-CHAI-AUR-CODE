0ddbaf352d590340c81799004afca50bb0eef6d7
Fuad <167557967+wait-fuad@users.noreply.github.com>
2024-09-01 09:12:55 +0000

    string details

---
+++ 01-Basics/05-strings.js
@@ -0,0 +1,17 @@
+const name = "Fuad"
+const repoCount = 50
+
+// console.log(name + " has " + repoCount + " repositories.");
+// string concatenation
+
+// console.log(`Hello, my name is ${name} and my repo count is ${repoCount}.`);
+// string interpolation
+
+
+const gameName = new String('Fuad')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+
+console.log(gameName.length);

b635f0c6d9bdae703e6a69821c6bb4531c93c2d9
Fuad <waitfuad@gmail.com>
2024-09-07 00:38:23 +0600

    control flow..

---
+++ 04-Control-Flow/01-one.js
@@ -0,0 +1,51 @@
+// if
+
+const isUserLoggedIn = true;
+const temperature = 41;
+
+// if (temperature === 40) {
+//   console.log("Less than 50");
+// } else {
+//   console.log("Temperature is greater than 50");
+// }
+
+// console.log("Execute");
+
+// <, >,  <=, >=, ==, !=, ===, !==
+
+// const score = 200;
+
+// if (score > 100) {
+//   var power = "fly";
+//   console.log(`User power ${power}`);
+// }
+
+// console.log(`User power ${power}`);
+
+// const balance = 1000;
+
+// if (balance > 500) console.log("Test"), // implicit scope
+// if (balance > 500) console.log("Test")
+
+// if (balance < 500) {
+//   console.log("Less than 500");
+// } else if (balance < 750) {
+//   console.log(" Less than 750");
+// } else if (balance < 900) {
+//   console.log("Less than 900");
+// } else {
+//   console.log("Less than 1200");
+// }
+
+const userLoggedIn = true;
+const debitCard = true;
+const loggedInFromGoogle = false;
+const loggedInFromEmail = true;
+
+if (userLoggedIn && debitCard && 2 == 2) {
+  console.log("Allow to buy course");
+}
+
+if (loggedInFromGoogle || loggedInFromEmail) {
+  console.log("User logged in");
+}
---
+++ 04-Control-Flow/02-switch.js
@@ -0,0 +1,30 @@
+// switch (key) {
+//     case value:
+
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "April";
+
+switch (month) {
+  case "Jan":
+    console.log("January");
+    break;
+  case "Feb":
+    console.log("February");
+    break;
+  case "April":
+    console.log("March");
+    break;
+  case 4:
+    console.log("April");
+    break;
+
+  default:
+    console.log("Default case match");
+
+    break;
+}
